b0d43cf603f3de03e58a498240d7785662de1453
ssy <sp.shiyang.shen@enflame-tech.com>
2021-01-07 19:14:55 +0800

    update CMakeLists.txt

---
+++ include/file.hpp
@@ -0,0 +1,4 @@
+#ifndef RDFIND_FILE_H
+#define RDFIND_FILE_H 1
+
+#endif // !RDFIND_FILE_H
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+// #include<filesystem>
+
+#include<file.hpp>
+
+int main(void){
+
+    std::cout<<"done"<<std::endl;
+    return 0;
+}
