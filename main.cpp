fcdf86fb267aaaeb244f3a0f1fdf6e918cd8db4a
Askr <m3933030@gmail.com>
2021-01-11 00:16:42 +0800

    2021/01/11

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include<iostream>
+#include<string>
+
 #include<boost/filesystem.hpp>
 
 #include<file.hpp>
 
-int main(void){
+int main(int argc,char *argv[]){
 
+    std::cout<<argc<<std::endl<<*argv<<std::endl;
+    std::cout<<"filesize is "<< boost::filesystem::file_size(argv[0])<<std::endl;
     std::cout<<"done"<<std::endl;
     return 0;
 }
