7f1e92d8a8df46948cd4f3b3957cdf115750e984
ssy <sp.shiyang.shen@enflame-tech.com>
2021-01-11 21:03:35 +0800

    update

--- main.cpp
+++ main.cpp
@@ -6,7 +6,10 @@
 #include<file.hpp>
 
 int main(int argc,char *argv[]){
-
+    for(int i=0;i<sizeof(argv);++i)
+    {
+        std::cout<<"args is "<<argv[i]<<std::endl;
+    }
     std::cout<<argc<<std::endl<<*argv<<std::endl;
     std::cout<<"filesize is "<< boost::filesystem::file_size(argv[0])<<std::endl;
     std::cout<<"done"<<std::endl;
