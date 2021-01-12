07c7f93359d4c78f96e1c3893bd63c96abf7286b
Askr <m3933030@gmail.com>
2021-01-13 00:22:03 +0800

    add openssl

--- main.cpp
+++ main.cpp
@@ -10,12 +10,15 @@ int main(int argc,char *argv[]){
 
     boost::filesystem::path root_path{argv[1]};
 
+    u_int32_t num=0;
     for (auto &_path :boost::filesystem::recursive_directory_iterator(root_path))
     {
-        std::cout<<"_file is "<<boost::filesystem::absolute(_path.path())<<std::endl;
+        ++num;
+        // std::cout<<"_file is "<<boost::filesystem::absolute(_path.path())<<std::endl;
     }
     std::cout<< "root path is "<<root_path<<std::endl;
     std::cout<<"filesize is "<< boost::filesystem::file_size(argv[0])<<std::endl;
+    std::cout<<"num is "<<num<<std::endl;
     std::cout<<"done"<<std::endl;
     return 0;
 }
