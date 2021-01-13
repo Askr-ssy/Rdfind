b696422c5fcaf02c2901b35e3ba7f9ddc54733e5
ssy <sp.shiyang.shen@enflame-tech.com>
2021-01-13 18:59:30 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,24 +1,26 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 
-#include<boost/filesystem.hpp>
+#include <boost/filesystem.hpp>
 
-#include<file.hpp>
+#include <find.hpp>
 
-int main(int argc,char *argv[]){
-    assert(boost::filesystem::exists(argv[1]));
+int main(int argc, char *argv[])
+{
+  assert(boost::filesystem::is_directory(argv[1]) && "the path is not directory");
 
-    boost::filesystem::path root_path{argv[1]};
-
-    u_int32_t num=0;
-    for (auto &_path :boost::filesystem::recursive_directory_iterator(root_path))
-    {
-        ++num;
-        // std::cout<<"_file is "<<boost::filesystem::absolute(_path.path())<<std::endl;
-    }
-    std::cout<< "root path is "<<root_path<<std::endl;
-    std::cout<<"filesize is "<< boost::filesystem::file_size(argv[0])<<std::endl;
-    std::cout<<"num is "<<num<<std::endl;
-    std::cout<<"done"<<std::endl;
-    return 0;
+  askr::find find{argv[1]};
+  std::cout<<"root path is "<< find.root_path()<<std::endl;
+  // askr::find find = new find(argv[1]);
+  u_int32_t num = 0;
+  for (auto &_path : boost::filesystem::recursive_directory_iterator(find))
+  {
+    ++num;
+    // std::cout<<"_file is "<<boost::filesystem::absolute(_path.path())<<std::endl;
+  }
+  std::cout << "root path is " << find << std::endl;
+  std::cout << "filesize is " << boost::filesystem::file_size(argv[0]) << std::endl;
+  std::cout << "num is " << num << std::endl;
+  std::cout << "done" << std::endl;
+  return 0;
 }
