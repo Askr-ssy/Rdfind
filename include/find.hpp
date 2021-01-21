e8e5811ff411c8c0dcb0d9d8b74025280a9d164d
Askr <m3933030@gmail.com>
2021-01-21 23:55:54 +0800

    update

--- include/find.hpp
+++ include/find.hpp
@@ -1,6 +1,9 @@
 #include <boost/filesystem.hpp>
 #include <string>
 #include <iostream>
+#include<unordered_map>
+#include<map>
+#include "md5.hpp"
 namespace askr
 {   
     class find : public boost::filesystem::path
@@ -8,16 +11,20 @@ namespace askr
     public:
         // find();
         find(const std::basic_string<value_type> &s):boost::filesystem::path(s){};
-    };
 
-    class ping
-    {
+        bool init(){
+            for (auto &_path : boost::filesystem::recursive_directory_iterator(this->native())){
+                if (boost::filesystem::is_directory(_path))
+                    continue;
+                else {
+                    askr::hash phash;
+                    // std::cout<<phash.get_md5hash(_path.path().native())<<std::endl;
+                    summary[_path.path().native()] = phash.get_md5hash(_path.path().native());
+                }
+            }
+        }
     public:
-        ping()
-        {
-            std::cout << "pong" << std::endl;
-        };
-        ~ping();
+        std::map<std::string,std::string> summary;
     };
 
 } // end namespace askr
--- main.cpp
+++ main.cpp
@@ -14,52 +14,12 @@ int main(int argc, char *argv[])
 
   askr::find find{argv[1]};
   std::cout << "root path is " << find.c_str() << std::endl;
-  // askr::find find = new find(argv[1]);
-  u_int32_t num = 0;
-  for (auto &_path : boost::filesystem::recursive_directory_iterator(find))
+  find.init();
+  for(auto &var : find.summary)
   {
-    ++num;
-    if (boost::filesystem::is_directory(_path))
-      continue;
-    else
-    {
-      askr::hash phash;
-
-      std::cout << "file is " << _path.path() << std::endl;
-      std::cout<<"md5 is " << phash.get_md5hash(_path.path().native())<<std::endl;
-
-      std::cout << "_filesize is " << boost::filesystem::file_size(_path.path()) << std::endl;
-    }
+    std::cout<<var.second <<" "<<var.first<<std::endl; 
   }
-  // std::cout << "filesize is " << boost::filesystem::file_size(argv[0]) << std::endl;
-  std::cout << "num is " << num << std::endl;
+
   std::cout << "done" << std::endl;
   return 0;
 }
-
-// using boost::uuids::detail::md5;
-
-// std::string toString(const md5::digest_type &digest)
-// {
-//     const auto charDigest = reinterpret_cast<const char *>(&digest);
-//     std::string result;
-//     boost::algorithm::hex(charDigest, charDigest + sizeof(md5::digest_type), std::back_inserter(result));
-//     return result;
-// }
-
-// int main ()
-// {
-//     std::string s;
-
-//     while(std::getline(std::cin, s)) {
-//         md5 hash;
-//         md5::digest_type digest;
-
-//         hash.process_bytes(s.data(), s.size());
-//         hash.get_digest(digest);
-
-//         std::cout << "md5(" << s << ") = " << toString(digest) << '\n';
-//     }
-
-//     return 0;
-// }
